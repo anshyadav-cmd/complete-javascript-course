2d4a606f0afec3a2e7fecde4f49b99ba83c7a2cb
anshyadav <anshyadav4244@gmail.com>
2023-02-15 14:43:19 +0530

    #2 project Model Window complete
    manipulating classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const model = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const showModal = function () {
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const hideModal = function () {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+document.querySelectorAll('.show-modal').forEach(val => {
+  val.addEventListener('click', showModal);
+});
+document.querySelector('.close-modal').addEventListener('click', hideModal);
+
+overlay.addEventListener('click', hideModal);
+
+document.body.addEventListener('keyup', e => {
+  if (e.key === 'Escape') {
+    if (!model.classList.contains('hidden')) {
+      hideModal();
+    }
+  }
+});
